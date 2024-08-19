becc9a0da1de4699c6b9890509254f07fd9f9748
Steven Tan <18841105+yen360@users.noreply.github.com>
2024-08-19 21:36:05 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: 'A Simple / Speedy / Secure Link Shortener with Analytics.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
